890bf1a905304af01bf26c03971c7ef9bdb37e60
Colin Tseng <klt.aus@yahoo.com>
2021-04-10 14:46:31 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: "Steve",
+      email: "steve@steve.com"
+    })
+  };
+// let body = document
+
+
 // Add your code here
+function submitData(name, email) {
+  return  fetch('http://localhost:3000/users', configurationObject)
+    .then(function(response) {
+      return response.json();
+  })
+  .then(function(json) {
+    json.name = name;
+    json.email = email;
+    let div = document.createElement('P');
+    let text = document.createTextNode(`${json.id}`); 
+    let userID = div.appendChild(text);
+    let stringID = (json.id).toString();
+    console.log(json);
+    return document.body.innerHTML = `${stringID}`
+    // return document.body.append(userID);
+  })
+  .catch(function(error) {
+    let errorMessage = error.message
+    let div = document.createElement('P');
+    let text = document.createTextNode(`${errorMessage}`); 
+    let userID = div.appendChild(text);
+    let stringID = (userID).toString();
+    return document.body.innerHTML = `${errorMessage}`
+    // return document.getElementById("body").appendChild(userID);
+  })
+}
